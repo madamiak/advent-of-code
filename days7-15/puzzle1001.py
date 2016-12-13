2c12140a8bfe17d2e908d78105b9d38e85c03a24
Mateusz Adamiak <mateusz.adamiak@nokia.com>
2016-12-13 17:05:32 +0100

    Incomplete day 10

--- days7-15/puzzle1001.py
+++ days7-15/puzzle1001.py
@@ -2,24 +2,106 @@ import re
 
 
 class Puzzle1001:
-    def __init__(self):
+    def __init__(self, search_pair):
+        self.search_pair = search_pair
         self.bots = {}
+        self.outputs = {}
         self.value_dist_regex = r'value (\d+) goes to bot (\d+)'
-        self.bot_gives_regex = r'value (\d+) goes to bot (\d+)'
-        pass
+        self.bot_obtains_regex = r'bot (\d+) gives (low|high) to bot (\d+) and (high|low) to bot (\d+)'
+        self.output_obtains_regex = r'bot (\d+) gives (low|high) to output (\d+) and (high|low) to output (\d+)'
 
     def solve(self, puzzle):
-        regex = re.compile(self.value_dist_regex)
+        # regex = re.compile(self.value_dist_regex)
         instructions = re.split('\n', puzzle)
-        if regex.search(puzzle) is not None:
-            match = regex.search(puzzle)
-            value = [int(match.group(1))]
-            bot_number = int(match.group(2))
-            self.bots[bot_number] = value
+        while len(instructions) > 0:
+            action_taken = False
+            for instruction in instructions[:]:
+                if re.search(self.value_dist_regex, instruction) is not None:
+                    action_taken = True
+                    match = re.search(self.value_dist_regex, instruction)
+                    value = int(match.group(1))
+                    bot_number = int(match.group(2))
+                    if self.bots.__contains__(bot_number):
+                        self.bots[bot_number].append(value)
+                    else:
+                        self.bots[bot_number] = [value]
+                elif re.search(self.bot_obtains_regex, instruction) is not None:
+                    action_taken = True
+                    match = re.search(self.bot_obtains_regex, instruction)
+                    source_bot = int(match.group(1))
+                    if len(self.bots[source_bot]) == 2:
+                        source_bot_values = sorted(self.bots[source_bot])
+                        if self.search_pair is not None:
+                            if self.search_pair == source_bot_values:
+                                return source_bot
+                        dest_bot1 = [match.group(2), int(match.group(3))]
+                        dest_bot2 = [match.group(4), int(match.group(5))]
+                        if dest_bot1[0] == 'high':
+                            if self.bots.__contains__(dest_bot1[1]):
+                                self.bots[dest_bot1[1]].append(source_bot_values[1])
+                            else:
+                                self.bots[dest_bot1[1]] = [source_bot_values[1]]
+                        else:
+                            if self.bots.__contains__(dest_bot1[1]):
+                                self.bots[dest_bot1[1]].append(source_bot_values[0])
+                            else:
+                                self.bots[dest_bot1[1]] = [source_bot_values[0]]
+                        if dest_bot2[0] == 'high':
+                            if self.bots.__contains__(dest_bot2[1]):
+                                self.bots[dest_bot2[1]].append(source_bot_values[1])
+                            else:
+                                self.bots[dest_bot2[1]] = [source_bot_values[1]]
+                        else:
+                            if self.bots.__contains__(dest_bot2[1]):
+                                self.bots[dest_bot2[1]].append(source_bot_values[0])
+                            else:
+                                self.bots[dest_bot2[1]] = [source_bot_values[0]]
+                        self.bots[source_bot] = []
+
+                elif re.search(self.output_obtains_regex, instruction) is not None:
+                    action_taken = True
+                    match = re.search(self.output_obtains_regex, instruction)
+                    source_bot = int(match.group(1))
+                    if len(self.bots[source_bot]) == 2:
+                        source_bot_values = sorted(self.bots[source_bot])
+                        if self.search_pair is not None:
+                            if self.search_pair == source_bot_values:
+                                return source_bot
+                        dest_bot1 = [match.group(2), int(match.group(3))]
+                        dest_bot2 = [match.group(4), int(match.group(5))]
+                        if dest_bot1[0] == 'high':
+                            if self.bots.__contains__(dest_bot1[1]):
+                                self.bots[dest_bot1[1]].append(source_bot_values[1])
+                            else:
+                                self.bots[dest_bot1[1]] = [source_bot_values[1]]
+                        else:
+                            if self.bots.__contains__(dest_bot1[1]):
+                                self.bots[dest_bot1[1]].append(source_bot_values[0])
+                            else:
+                                self.bots[dest_bot1[1]] = [source_bot_values[0]]
+                        if dest_bot2[0] == 'high':
+                            if self.bots.__contains__(dest_bot2[1]):
+                                self.bots[dest_bot2[1]].append(source_bot_values[1])
+                            else:
+                                self.bots[dest_bot2[1]] = [source_bot_values[1]]
+                        else:
+                            if self.bots.__contains__(dest_bot2[1]):
+                                self.bots[dest_bot2[1]].append(source_bot_values[0])
+                            else:
+                                self.bots[dest_bot2[1]] = [source_bot_values[0]]
+                        self.bots[source_bot] = []
+
+                if action_taken:
+                    instructions.remove(instruction)
+            if not action_taken:
+                instructions = []
         pass
 
     def get_bot_with_values(self, values):
-        return {k: v for k, v in self.bots.iteritems() if v[0] == values[0]}.keys()[0]
+        return {k: v for k, v in self.bots.iteritems() if v == values or v[::-1] == values}.keys()
 
     def get_bot_values(self, bot_number):
         return self.bots.get(bot_number)
+
+    def get_output_values(self, output_number):
+        return self.outputs.get(output_number)
--- days7-15/test_puzzle1001.py
+++ days7-15/test_puzzle1001.py
@@ -7,11 +7,11 @@ class TestPuzzle1001(TestCase):
     pass
 
     def setUp(self):
-        self.test_object = Puzzle1001()
+        self.test_object = Puzzle1001(None)
 
     def test_distribute_value(self):
         self.test_object.solve("value 5 goes to bot 2")
-        self.assertEqual(self.test_object.get_bot_with_values([5]), 2)
+        self.assertEqual(self.test_object.get_bot_with_values([5]), [2])
 
     def test_skip_instruction_when_no_values(self):
         self.test_object.solve("bot 2 gives low to bot 1 and high to bot 0")
@@ -22,10 +22,25 @@ class TestPuzzle1001(TestCase):
                                "bot 2 gives low to bot 1 and high to bot 0")
         self.assertEqual(self.test_object.get_bot_values(2), [5])
 
+    def test_distribute_two_values(self):
+        self.test_object.solve("value 5 goes to bot 1\n"
+                               "value 5 goes to bot 2\n"
+                               "value 4 goes to bot 2\n")
+        self.assertEqual(self.test_object.get_bot_with_values([5, 4]), [2])
+        self.assertEqual(self.test_object.get_bot_with_values([5]), [1])
+
     def test_bot_gives_value_when_two_available(self):
         self.test_object.solve("value 5 goes to bot 2\n"
                                "value 4 goes to bot 2\n"
                                "bot 2 gives low to bot 1 and high to bot 0")
-        self.assertEqual(self.test_object.get_bot_values(0), [])
-        self.assertEqual(self.test_object.get_bot_values(1), 4)
-        self.assertEqual(self.test_object.get_bot_values(2), 5)
+        self.assertEqual(self.test_object.get_bot_values(1), [4])
+        self.assertEqual(self.test_object.get_bot_values(0), [5])
+        self.assertEqual(self.test_object.get_bot_values(2), [])
+
+    def test_putting_to_output(self):
+        self.test_object.solve("value 5 goes to bot 2\n"
+                               "value 4 goes to bot 2\n"
+                               "bot 2 gives low to output 1 and high to output 0")
+        self.assertEqual(self.test_object.get_output_values(1), [4])
+        self.assertEqual(self.test_object.get_output_values(0), [5])
+        self.assertEqual(self.test_object.get_bot_values(2), [])
