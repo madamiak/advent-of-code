76c32ea924a192fb4765c33cf4f2954404df065e
Mateusz Adamiak <mateusz.adamiak@nokia.com>
2016-12-21 08:46:38 +0100

    Complete day 20

--- days7-25/test_puzzle20.py
+++ days7-25/test_puzzle20.py
@@ -9,8 +9,21 @@ class TestPuzzle20(TestCase):
     def test_simple(self):
         self.assertEqual(Puzzle20.solve("5-8\n"
                                         "0-2\n"
-                                        "4-7"), 3)
+                                        "4-7", 1), 3)
+
+    def test_overlap(self):
+        self.assertEqual(Puzzle20.solve("5-8\n"
+                                        "0-7\n"
+                                        "4-7", 1), 9)
+
+    def test_neighbour(self):
+        self.assertEqual(Puzzle20.solve("5-8\n"
+                                        "0-4", 1), 9)
 
     def test(self):
         with open('Puzzle20.txt', 'r') as puzzle:
-            Puzzle20.solve(puzzle.read())
+            print Puzzle20.solve(puzzle.read(), 1)
+
+    def test_2(self):
+        with open('Puzzle20.txt', 'r') as puzzle:
+            print Puzzle20.solve(puzzle.read(), 2)
